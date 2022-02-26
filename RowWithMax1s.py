8d50600b82d1035fd08baf720b5b0128cc8b70c3
razhakhan <65012840+razhakhan@users.noreply.github.com>
2022-02-26 11:28:40 +0530

    Create RowWithMax1s.py

---
+++ RowWithMax1s.py
@@ -0,0 +1,63 @@
+#User function Template for python3
+class Solution:
+
+    def rowWithMax1s(self,arr, n, m):
+        index=m-1
+        maxrow=0
+        for i in range(n):
+            flag=False
+            while(index>=0 and arr[i][index]==1):
+                flag=True
+                index-=1
+            if(flag):
+                maxrow=i
+        if(maxrow==0 and arr[0][m-1]==0):
+            return -1
+        return maxrow
+                
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, m = list(map(int, input().strip().split()))
+        
+        inputLine = list(map(int, input().strip().split()))
+        arr = [[0 for j in range(m)] for i in range(n)]
+        
+        for i in range(n):
+            for j in range(m):
+                arr[i][j] = inputLine[i * m + j]
+        ob = Solution()
+        ans = ob.rowWithMax1s(arr, n, m)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
+
+"""
+Example 1:
+
+Input: 
+N = 4 , M = 4
+Arr[][] = {{0, 1, 1, 1},
+           {0, 0, 1, 1},
+           {1, 1, 1, 1},
+           {0, 0, 0, 0}}
+Output: 2
+Explanation: Row 2 contains 4 1's (0-based
+indexing).
+
+Example 2:
+
+Input: 
+N = 2, M = 2
+Arr[][] = {{0, 0}, {1, 1}}
+Output: 1
+Explanation: Row 1 contains 2 1's (0-based
+indexing).
+
+"""
